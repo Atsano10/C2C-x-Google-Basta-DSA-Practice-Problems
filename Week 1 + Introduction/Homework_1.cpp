86cd1e7a38e19b8ef32f8fa62fbbd479d6a02734
Apostolos Tsano <tolitsano18@gmail.com>
2026-07-11 02:55:43 -0400

    Added file structured and the work for day 1

---
+++ Week 1 + Introduction/Homework_1.cpp
@@ -0,0 +1,16 @@
+/*
+Given an array of integers and an integer k, return the total number of subarrays whose sum equals k. A subarray is a contiguous non-empty sequence of elements within an array.
+
+Input : N = 4, array[] = {3, 1, 2, 4}, k = 6
+Output: 2
+Explanation: The subarrays that sum up to 6 are [3, 1, 2] and [2, 4].
+
+[execution time limit] 0.5 seconds (cpp)       |       [memory limit] 2g
+*/
+
+#include <iostream>
+
+int main() {
+  
+    return 0;
+}
