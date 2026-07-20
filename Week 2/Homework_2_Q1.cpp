0b9def62fbf0092476d96db4e6decfc45578c900
Apostolos Tsano <tolitsano18@gmail.com>
2026-07-19 23:13:59 -0400

    add homeworks

---
+++ Week 2/Homework_2_Q1.cpp
@@ -0,0 +1,15 @@
+/*
+Remove the first element matching a given key from a linked list.
+
+Example:
+Input:
+linkedList: [1,5,3,2,1]
+key: 3
+Output: [1,5,2,1]
+*/
+
+#include <iostream>
+
+int main() {
+
+}
---
+++ Week 2/Homework_2_Q2.cpp
@@ -0,0 +1,31 @@
+/*
+You are given two non-empty arrays representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as an array. You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Example 1:
+Input: arr1 = [2,4,3], arr2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807.
+
+Example 2:
+Input: arr1 = [0], arr2 = [0]
+Output: [0]
+
+[execution time limit] 0.5 seconds (cpp)
+
+[memory limit] 1 GB
+
+[input] array.integer arr1
+
+[input] array.integer arr2
+
+[output] array.integer
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> solution(vector<int> arr1, vector<int> arr2) {
+
+}
+
