bfbd5637147d10a317a554866ceb2c56c6db9ab6
Apostolos Tsano <tolitsano18@gmail.com>
2026-07-19 23:46:21 -0400

    start working on both problems

--- Week 2/Homework_2_Q1.cpp
+++ Week 2/Homework_2_Q1.cpp
@@ -8,7 +8,30 @@ key: 3
 Output: [1,5,2,1]
 */
 
+// Time start 11:15pm - paused at 11:37.
+// Thought process just iterate through the linkedlist until next == null and 
+// remove if we find the key. 
+
+// Just setting up the function and we can test it somehow else.
+
 #include <iostream>
+
+struct Node{
+    int data;
+    Node* next;
+};
+
+Node* remove(Node* head, int key){
+    Node* temp = head;
+    while(head->next != nullptr){
+        if(head->data == key){
+
+        }
+        head = head->next;
+    }
+
+    return head;
+}
 
 int main() {
 
--- Week 2/Homework_2_Q2.cpp
+++ Week 2/Homework_2_Q2.cpp
@@ -1,5 +1,7 @@
 /*
-You are given two non-empty arrays representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as an array. You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+You are given two non-empty arrays representing two non-negative integers. The digits are stored in reverse order, 
+and each of their nodes contains a single digit. Add the two numbers and return the sum as an array. 
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 
 Example 1:
 Input: arr1 = [2,4,3], arr2 = [5,6,4]
@@ -21,11 +23,29 @@ Output: [0]
 [output] array.integer
 */
 
+
+//Time started 11:38 - 
+// Thought process, we need to re order the numbers correctly, keep count of "carrys" and add each corresponding index.
+
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 vector<int> solution(vector<int> arr1, vector<int> arr2) {
+    reverse(arr1.begin(),arr1.end());
+    reverse(arr2.begin(),arr2.end());
 
+    int carry = 0;
+    vector<int> result;
+
+    for(int i = 0; i < arr1.size() && i < arr2.size(); i++){
+        if(arr1[i] + arr2[i] > 10){
+            
+        }
+        result.push_back(arr1[i]+arr2[i]);
+    }
+
+    return result;
 }
 
